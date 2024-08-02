0630fe44b7953e84f95356b552c0028afea2a8e3
Anahita Malviya <138990422+anahitaaz@users.noreply.github.com>
2024-08-02 19:56:31 +0530

    Add files via upload

---
+++ spellingchecker.py
@@ -0,0 +1,20 @@
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+root=Tk()
+root.title("Spelling Checker")
+root.geometry("700x400")
+root.config(background="#dae6f6")
+heading=Label(root,text="Spelling Checker",font=("Trebuchet MS",30,"bold"),bg="#dae6f6",fg="#364971")
+heading.pack(pady=(50,0))
+
+
+enter_text=Entry(root,justify="center",width=30,font=("poppins",25),bg="white",border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
+button=Button(root,text='Check',font=('arial',20,'bold'),fg='white',bg='red')
+button.pack()
+
+
+root.mainloop()
